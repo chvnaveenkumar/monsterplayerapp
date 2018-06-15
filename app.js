8ae53c914f5643ebbcda7e78a8074f22d7adfde4
Chandaluri <S530742@nwmissouri.edu>
2018-06-15 14:49:09 -0500

    Modified attack and special attack

--- app.js
+++ app.js
@@ -15,11 +15,19 @@ new Vue({
         },
         attack: function()
         {
-
+            this.monsterHealth -= this.calculateDamage(3,10);
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
         },
         specialAttack: function()
         {
-
+            this.monsterHealth -= this.calculateDamage(10,20);
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
         },
         heal: function()
         {
@@ -28,6 +36,36 @@ new Vue({
         giveup: function()
         {
             
+        },
+        monsterAttacks: function()
+        {
+            this.playerHealth -= this.calculateDamage(5,12);
+            this.checkWin();
+        },
+        calculateDamage: function(min,max){
+            return Math.max(Math.floor(Math.random()*max)+1,min);
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0){
+                if(confirm("You won!! Want to Continue?"))
+                {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }else if(this.playerHealth <= 0)
+            {
+                if(confirm('You lost!! New Game?'))
+                {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+
         }
 
     }
